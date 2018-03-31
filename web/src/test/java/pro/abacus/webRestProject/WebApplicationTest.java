96ce02f57748ec97127005546831f597251a624b
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-03-31 17:51:49 +0200

    Changed WebApplicationTest

---
+++ web/src/test/java/pro/abacus/webRestProject/WebApplicationTest.java
@@ -0,0 +1,16 @@
+package pro.abacus.webRestProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//Check that the Spring application context can be loaded successfully 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
