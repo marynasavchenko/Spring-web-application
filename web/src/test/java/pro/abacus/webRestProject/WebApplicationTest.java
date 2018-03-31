39ccb7c5cc71d39b6566057afd3577cb8b57cf15
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-03-31 18:05:21 +0200

    changed jog4j.roperties

--- web/src/test/java/pro/abacus/webRestProject/WebApplicationTest.java
+++ web/src/test/java/pro/abacus/webRestProject/WebApplicationTest.java
@@ -5,12 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//Check that the Spring application context can be loaded successfully 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApplicationTest {
 
 	@Test
-	public void contextLoads() {
+	public void applicationContextShouldBeLoadedSuccessfully() {
 	}
 }
