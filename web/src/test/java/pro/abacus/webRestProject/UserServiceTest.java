d581ff15ef22988bce99f2bbf60992bc291b1907
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-04-02 14:03:24 +0200

    UserServiceTest setRoles verifies

--- web/src/test/java/pro/abacus/webRestProject/UserServiceTest.java
+++ web/src/test/java/pro/abacus/webRestProject/UserServiceTest.java
@@ -5,7 +5,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,21 +13,17 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import pro.abacus.webRestProject.models.Role;
 import pro.abacus.webRestProject.models.User;
 import pro.abacus.webRestProject.repositories.RoleRepository;
 import pro.abacus.webRestProject.repositories.UserRepository;
 import pro.abacus.webRestProject.services.UserService;
 import pro.abacus.webRestProject.services.UserServiceImplementation;
 
-//TODO roles
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 	private static final String UNSECURED_PASSWORD = "1234test";
 	private static final String SECURED_PASSWORD = "1BC29B36F623BA82AAF6724FD3B16718";
-    private static final Set<Role> SET_OF_ROLES = new HashSet<>();
     private static final String ANY_USER_NAME = "Anton";
-	
 	
 	private UserService userService;
 	
@@ -44,13 +39,10 @@ public class UserServiceTest {
 	@Mock
 	private User user;
 	
-	@Mock
-	private Role userRole;
 	
 	@Before
 	public void setup(){
 	userService = new UserServiceImplementation(userRepository, roleRepository, encoder);
-	SET_OF_ROLES.add(userRole);
 	}
 	
 	@Test
@@ -60,7 +52,7 @@ public class UserServiceTest {
 		when(encoder.encode(UNSECURED_PASSWORD)).thenReturn(SECURED_PASSWORD);
 		
 		userService.save(user);
-		//verify(user).setRoles(SET_OF_ROLES); does not work 
+		verify(user).setRoles(new HashSet<>()); 
 		verify(user).setPassword(SECURED_PASSWORD);
 		verify(userRepository).save(user);
 	}
@@ -70,7 +62,6 @@ public class UserServiceTest {
 		when(user.getName()).thenReturn(ANY_USER_NAME);
 		when(userRepository.existsByName(ANY_USER_NAME)).thenReturn(true);
 		
-		//shold do this assert or not?
 		assertTrue(userService.isDuplicate(user));
 		verify(userRepository).existsByName(ANY_USER_NAME);
 		
