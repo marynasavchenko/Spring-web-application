c242a042858a713222a26f36e949355f07f83f83
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-05 11:39:45 +0100

    deleted SpringBootServletInitializer and configure(),packaged to jar

--- src/main/java/pro/abacus/webproject/WebApplication.java
+++ src/main/java/pro/abacus/webproject/WebApplication.java
@@ -2,20 +2,11 @@ package pro.abacus.webproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebApplication.class);
-	}
+public class WebApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(WebApplication.class, args);
 	}
-
 }
