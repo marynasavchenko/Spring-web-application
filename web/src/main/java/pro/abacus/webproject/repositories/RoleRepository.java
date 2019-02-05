53ec18a75f770e88f42d2d93c4c44c6e96a38cf6
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-05 10:10:53 +0100

    changed folder structure

--- web/src/main/java/pro/abacus/webproject/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package pro.abacus.webproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pro.abacus.webproject.domain.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
